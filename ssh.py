d84946e592416ee5cadd1fc9b86648f132d87abe
darius <dariusnew@pulsesecure.com>
2026-01-13 14:07:26 +0800

    added error printing

--- ssh.py
+++ ssh.py
@@ -10,21 +10,24 @@ class SSHTester():
         self.passkeys = pass_list
         self.targets_dict = {}
         self.targets_creds = {}
+        self.targets_error = {}
         self.num_threads = num_threads
 
     def test_ssh(self, host: str):
         ssh = paramiko.SSHClient()
         ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-
+        self.targets_dict[host] = False
+        self.targets_error[host] = []
         for username in self.users:
             for passkey in self.passkeys:
-                if host not in self.targets_dict.keys():
+                if not self.targets_dict[host]:
                     try:
-                        ssh.connect(host, username=username, password=passkey)
+                        ssh.connect(host, username=username, password=passkey, timeout=10)
                         self.targets_dict[host] = True
                         self.targets_creds[host] = (username, passkey)
                     except (paramiko.ssh_exception.BadHostKeyException, paramiko.ssh_exception.AuthenticationException, paramiko.ssh_exception.UnableToAuthenticate, socket.error) as e:
                         self.targets_dict[host] = False
+                        self.targets_error[host].append(e)
         
     
     def run(self):
@@ -51,3 +54,4 @@ class SSHTester():
                 print(f"--- SSH client {target} is able to authenticate with {self.targets_creds[target][0]}:{self.targets_creds[target][1]}---")
             else:
                 print(f"--- SSH client {target} is unable to authenticate ---")
+                print(self.targets_error[target])
